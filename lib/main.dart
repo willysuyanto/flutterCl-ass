0989aa75601cec49288dfcd6f0139d33c66dd24a
Willy Suyanto <willy.suyanto@c-iao.com>
2021-02-22 20:18:16 +0700

    new task

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,41 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HalamanAwal extends StatelessWidget {
+class HalamanAwal extends StatefulWidget {
+  HalamanAwal({Key key}) : super(key: key);
+
+  @override
+  HalamanAwalState createState() => HalamanAwalState();
+}
+
+class HalamanAwalState extends State<HalamanAwal> {
+
+  List<Widget> _loop() {
+    List<Widget> list = [
+    ];
+
+    for(int i = 0; i <= 10; i++){
+      print(i);
+      list.add(
+      Container(
+        height: 150,
+        child: Text('Text-$i'),
+      )
+    );
+    }
+    return list;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Halaman Awal"),
         actions: [
-          IconButton(icon: Icon(Icons.home), onPressed: () {},),
+          IconButton(
+            icon: Icon(Icons.home),
+            onPressed: () {},
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -36,104 +63,10 @@ class HalamanAwal extends StatelessWidget {
         color: Colors.grey,
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: Wrap(
-            direction: Axis.horizontal,
-            alignment: WrapAlignment.spaceAround,
-            runSpacing: 16,
-            children: [
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.teal,
-                child: Center(
-                  child: Text("ini text"),
-                )
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.red,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Text 1"),
-                      Text("Text 2"),
-                    ],
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.blue,
-                child: FlutterLogo(
-
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.blue,
-                child: FlutterLogo(
-
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.blue,
-                child: FlutterLogo(
-
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.blue,
-                child: FlutterLogo(
-
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.blue,
-                child: FlutterLogo(
-
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.blue,
-                child: FlutterLogo(
-
-                ),
-              ), Container(
-                height: 100,
-                width: 100,
-                color: Colors.blue,
-                child: FlutterLogo(
-                ),
-              ),
-              // Container(
-              //   height: 200,
-              //   width: 200,
-              //   color: Colors.green,
-              //   child: Image.asset('assets/images/gambar1.jpg', fit: BoxFit.cover,),
-              // ),
-              Container(
-                height: 200,
-                width: 300,
-                child: Card(
-                  child: Image.network('https://images.unsplash.com/photo-1600371288342-3a16d67f7e7d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80')
-                ),
-              )
-            ],
-          ),
+        child: ListView(
+          children: _loop()
+        )
       ),
     );
   }
 }
-
-
